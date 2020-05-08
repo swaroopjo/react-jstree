c32dd522692b942afb99e0d79c29a9168b4f87ee
jyothiswaroop.pamalla <swaroop_jo@yahoo.com>
2020-05-07 19:00:10 -0500

    Remove CustomHook.

--- src/components/JsTree.js
+++ src/components/JsTree.js
@@ -1,17 +1,53 @@
 import React, { useRef, useImperativeHandle, forwardRef } from "react";
+import { useEffect } from "react";
 
-import useJsTree from "../custom-hooks/use-js-tree"
-
+import $ from "jquery";
+// eslint-disable-next-line
+import jstree from "jstree";
 const JsTree = forwardRef((props, ref) => {
-  const jsTreeRef = useRef();
-  const jsTreeHook = useJsTree(props.data, props.config, jsTreeRef);
+  const jsTreeRef = useRef(null);
+
+  const appendJstreeCss = () => {
+    const cssFiles = [
+      "https://cdnjs.cloudflare.com/ajax/libs/jstree/3.2.1/themes/default/style.min.css"
+    ];
+
+    cssFiles.forEach(css => {
+      let tag = document.createElement("link");
+      tag.setAttribute("href", css);
+      tag.setAttribute("rel", "stylesheet");
+      document.head.insertBefore(tag, document.head.childNodes[0]);
+    });
+  };
+
+  useEffect(() => {
+    appendJstreeCss();
+    $(jsTreeRef.current).jstree({
+      core: {
+        data: props.data,
+        themes: {
+          dots: false
+        }
+      },
+      plugins: ["search", "unique", "dnd", "types", "contextmenu"]
+    });
+  }, [props.data, jsTreeRef]);
 
   useImperativeHandle(ref, () => ({
-    openFirstNode() {
-      jsTreeHook.open_node("root_node_1");
+    openFirstNode(id) {
+      $(jsTreeRef.current).jstree("open_node", id);
     },
-    closeFirstNode() {
-      jsTreeHook.close_node("root_node_1");
+    closeFirstNode(id) {
+      $(jsTreeRef.current).jstree("close_node", id);
+    },
+    onSelectNode(callback) {
+      $(jsTreeRef.current).on("select_node.jstree", callback);
+    },
+    openAll() {
+      $(jsTreeRef.current).jstree("open_all");
+    },
+    closeAll() {
+      $(jsTreeRef.current).jstree("close_all");
     }
   }));
   return <div ref={jsTreeRef} />;
--- src/demo/DemoTree.js
+++ src/demo/DemoTree.js
@@ -1,19 +1,22 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import JsTree from "../components/JsTree";
 import treeData from "./sample-data/tree.data.json";
 
 export default function() {
-  const jsTreeRef = useRef();
+    const jsTreeRef = useRef();
+    const onSelectNode = (event, node) => {
+        console.log("Node selected: ", node);
+        console.log("Event: ", event);
+    };
+    useEffect(() => {
+        jsTreeRef.current.onSelectNode(onSelectNode);
+    }, []);
 
-  return (
-    <>
-      <button onClick={() => jsTreeRef.current.openFirstNode()}>
-        Open first node
-      </button>
-      <button onClick={() => jsTreeRef.current.closeFirstNode()}>
-        Close first node
-      </button>
-      <JsTree ref={jsTreeRef} data={treeData.data} />
-    </>
-  );
+    return (
+        <>
+            <button onClick={() => jsTreeRef.current.openAll()}>Open All</button>
+            <button onClick={() => jsTreeRef.current.closeAll()}>Close All</button>
+            <JsTree ref={jsTreeRef} data={treeData.data} />
+        </>
+    );
 }
