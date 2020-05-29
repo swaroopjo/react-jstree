19a921d91549cefb6a76bef0b4c75a7c6e8bb394
jyothiswaroop.pamalla <swaroop_jo@yahoo.com>
2020-05-29 13:58:54 -0500

    chages

--- src/demo/DemoTree.js
+++ src/demo/DemoTree.js
@@ -1,62 +1,114 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState, useCallback, useMemo } from "react";
 import JsTree from "../components/JsTree";
 import treeData from "./sample-data/tree.data.json";
 
 const plugins = ["search", "unique", "dnd", "types", "contextmenu"];
 
+const MemoizedTree = React.memo((props) => {
+    console.log("Rendering child component");
+    return <JsTree ref={props.jsTreeRef} config={props.config} />;
+});
 
 export default function() {
+    const [someText, setSomeText] = useState('');
+    const [ready, setReady] = useState(false);
     const jsTreeRef = useRef(null);
-
-    const isDragAllowed = (nodes) => {
+    const config = useRef({});
+    const isDragAllowed = useCallback((nodes) => {
         let rtn = true;
         nodes.forEach(function (obj) {
             if (obj.children && obj.children.length > 0)
                 rtn = false;
         });
         return rtn;
-    };
-
-    const isFirstNodeDisabled = () => {
-        jsTreeRef.current.call('is_disabled',
-            treeData.data[0],
-            (isDisabled) => {
-                console.log('First node disabled: '+isDisabled);
-            });
-    };
-
-    const jsTreeConfig = {
-        core: {
-            data: treeData.data,
-            themes: {
-                dots: false
-            }
-        },
-        plugins: plugins,
-        dnd: {
-            is_draggable : isDragAllowed
-        }
-    };
-
-    const onSelectNode = (event, node) => {
+    }, []);
+    const isFirstNodeDisabled = useCallback(() => {
+        const disabled = jsTreeRef.current.isDisabled(treeData.data[0]);
+        console.log('disabled: '+ disabled);
+    }, []);
+    const onSelectNode = useCallback((event, node) => {
         console.log("Node selected: ", node);
         console.log("Event: ", event);
-    };
-
-    useEffect(() => {
-        jsTreeRef.current.on("select_node.jstree", onSelectNode);
-        return () => {
-            jsTreeRef.current.trigger('destroy');
-        }
     }, []);
 
+    const customMenu = useCallback((node) => {
+        console.log('custom menu node:'+node);
+        if(node.original.type === 'I'){
+            return {
+                "firstMenu" : {
+                    "label" : "First Menu",
+                    "action" : function (node) {console.log('Click first menu'); }
+                },
+                "secondMenu" : {
+                    "label" : "Second Menu",
+                    "action" : function (node) { console.log('Click second menu'); }
+                },
+            }
+        }
+        else {
+            return {
+                "firstMenu" : {
+                    "label" : "First Menu",
+                    "action" : function (node) {console.log('Click first menu'); }
+                },
+                "secondMenu" : {
+                    "label" : "Second Menu",
+                    "action" : function (node) { console.log('Click second menu'); },
+                    "_disabled": true
+                },
+                "thirdMenu" : {
+                    "label" : "Third Menu",
+                    "action" : function (node) { console.log('Click third menu'); }
+                },
+            };
+        }
+    },[]);
+
+    useEffect(() => {
+        config.current = {
+            core: {
+                data: treeData.data,
+                themes: {
+                    dots: false
+                }
+            },
+            plugins: plugins,
+            dnd: {
+                is_draggable : isDragAllowed
+            },
+            contextmenu : {
+                items : customMenu
+            }
+        };
+        setReady(true);
+    }, []);
+
+    useEffect(() => {
+        console.log('Running tree event registration effect');
+        if(ready && jsTreeRef.current !== null){
+            jsTreeRef.current.onSelectNode(onSelectNode);
+            return () => {
+                console.log('running destroy');
+                jsTreeRef.current.destroy();
+            }
+        }
+    }, [ready]);
+
+    const openAll = useCallback(() => {
+        jsTreeRef.current.openAll();
+    }, []);
+    const closeAll = useCallback(() => {
+        jsTreeRef.current.closeAll();
+    }, []);
     return (
         <>
-            <button onClick={() => jsTreeRef.current.trigger('open_all')}>Open Alls</button>
-            <button onClick={() => jsTreeRef.current.trigger('close_all')}>Close All</button>
+            <button onClick={openAll}>Open All</button>
+            <button onClick={closeAll}>Close All</button>
             <button onClick={isFirstNodeDisabled}>Is First Node Disabled</button>
+            {
+                ready && <MemoizedTree jsTreeRef={jsTreeRef} config={config.current} />
+            }
 
-            <JsTree ref={jsTreeRef} config={jsTreeConfig} />
         </>
     );
 }
